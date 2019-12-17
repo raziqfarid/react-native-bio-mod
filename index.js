2b46de4af4207278828f2e9920e40952a55050a5
raziqfarid <raziqfarid@gmail.com>
2019-12-17 17:45:34 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import { NativeModules } from 'react-native';
 
-module.exports = NativeModules.BioMod;
+const { BioMod } = NativeModules
+
+export default {
+  sampleMethod: (strArg, numPar) => {
+    return BioMod.sampleMethod(strArg, numPar)
+  }
+}
